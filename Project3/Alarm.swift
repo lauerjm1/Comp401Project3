75cf8da3d9752e1ca54b33c9ae16b6aa54caba88
Jon Lauer <JonLauer@williams-air.home>
2016-04-22 00:53:10 -0400

    Cleaned up warnings about re-use identifiers. Added tableViewController subclasses. Added skeleton data classes, with comments for suggested fixes.

---
+++ Project3/Alarm.swift
@@ -0,0 +1,20 @@
+//
+//  Alarm.swift
+//  Project3
+//
+//  Created by Jon Lauer on 4/22/16.
+//  Copyright © 2016 Jon Lauer. All rights reserved.
+//
+
+import UIKit
+
+class Alarm {
+
+    var time: Int!
+    var daysOfWeek: [String]!
+    var activated: Bool!
+    var title: String!
+    // TODO: - Figure out if this is Double or some other type
+    var volume: Int!
+
+}
---
+++ Project3/Reminder.swift
@@ -0,0 +1,19 @@
+//
+//  Reminder.swift
+//  Project3
+//
+//  Created by Jon Lauer on 4/22/16.
+//  Copyright © 2016 Jon Lauer. All rights reserved.
+//
+
+import UIKit
+
+class Reminder {
+
+    // FIXME: - Should this be a date? I couldn't remember.
+    var time: Int!
+    var title: String!
+    // FIXME: - Again, double or int?
+    var volume: Int!
+
+}
