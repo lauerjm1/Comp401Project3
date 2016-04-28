8ea09b26ead02df7e9e653e33461587670d4a1e0
Michael Boom <michaelboom@2ce4250jgl.gcc.edu>
2016-04-27 20:37:33 -0400

    start of models, singletons, file saving, etc.

--- Project3/Alarm.swift
+++ Project3/Alarm.swift
@@ -8,7 +8,7 @@
 
 import UIKit
 
-class Alarm {
+class Alarm: NSObject {
 
     var time: Int!
     var daysOfWeek: [String]!
@@ -17,4 +17,57 @@ class Alarm {
     // TODO: - Figure out if this is Double or some other type
     var volume: Int!
 
+}
+
+class AlarmRepo {
+    var list = Dictionary<String,Dictionary<String,AnyObject>>()
+    let libPath:NSString = NSSearchPathForDirectoriesInDomains(.LibraryDirectory, .UserDomainMask, true)[0]
+    
+    static let singleton = AlarmRepo()
+    
+    private init() {
+        let pathName = libPath.stringByAppendingPathComponent("alarms.dat")
+        if NSFileManager.defaultManager().fileExistsAtPath(pathName) {
+            let dat = NSData(contentsOfFile: pathName)
+            do {
+                let obj = try NSJSONSerialization.JSONObjectWithData(dat!, options: NSJSONReadingOptions.MutableContainers) as! Dictionary<String,Dictionary<String,AnyObject>>
+                list = obj
+            } catch {}
+        }
+    }
+    
+    func interpretAlarm(dict:Dictionary<String,AnyObject>) -> Alarm {
+        let alm = Alarm()
+        for(k,v) in dict {
+            if alm.respondsToSelector(NSSelectorFromString(k)) {
+                alm.setValue(v, forKey: k)
+            }
+        }
+        return alm
+    }
+    
+    func addAlarmToRepo(name:String,alm:Alarm) {
+        list.removeValueForKey(name)
+        var keyVal = Dictionary<String,AnyObject>()
+        keyVal.updateValue(alm.time, forKey: "time")
+        keyVal.updateValue(alm.daysOfWeek, forKey: "daysOfWeek")
+        keyVal.updateValue(alm.activated, forKey: "activated")
+        keyVal.updateValue(alm.title, forKey: "title")
+        keyVal.updateValue(alm.volume, forKey: "volume")
+        list.updateValue(keyVal, forKey: name)
+        let pathName = libPath.stringByAppendingPathComponent("alarms.dat")
+        do {
+            let data = try NSJSONSerialization.dataWithJSONObject(list, options: NSJSONWritingOptions())
+            data.writeToFile(pathName, atomically: true)
+        } catch {}
+    }
+    
+    func deleteAlarmFromRepo(name:String) {
+        list.removeValueForKey(name)
+        let pathName = libPath.stringByAppendingPathComponent("alarms.dat")
+        do {
+            let data = try NSJSONSerialization.dataWithJSONObject(list, options: NSJSONWritingOptions())
+            data.writeToFile(pathName, atomically: true)
+        } catch {}
+    }
 }
--- Project3/Reminder.swift
+++ Project3/Reminder.swift
@@ -8,7 +8,7 @@
 
 import UIKit
 
-class Reminder {
+class Reminder: NSObject {
 
     // FIXME: - Should this be a date? I couldn't remember.
     var time: Int!
@@ -16,4 +16,55 @@ class Reminder {
     // FIXME: - Again, double or int?
     var volume: Int!
 
+}
+
+class ReminderRepo {
+    var list = Dictionary<String,Dictionary<String,AnyObject>>()
+    let libPath:NSString = NSSearchPathForDirectoriesInDomains(.LibraryDirectory, .UserDomainMask, true)[0]
+    
+    static let singleton = ReminderRepo()
+    
+    private init() {
+        let pathName = libPath.stringByAppendingPathComponent("reminders.dat")
+        if NSFileManager.defaultManager().fileExistsAtPath(pathName) {
+            let dat = NSData(contentsOfFile: pathName)
+            do {
+                let obj = try NSJSONSerialization.JSONObjectWithData(dat!, options: NSJSONReadingOptions.MutableContainers) as! Dictionary<String,Dictionary<String,AnyObject>>
+                list = obj
+            } catch {}
+        }
+    }
+    
+    func interpretReminder(dict:Dictionary<String,AnyObject>) -> Reminder {
+        let rem = Reminder()
+        for (k,v) in dict {
+            if rem.respondsToSelector(NSSelectorFromString(k)) {
+                rem.setValue(v, forKey: k)
+            }
+        }
+        return rem
+    }
+    
+    func addReminderToRepo(name:String,rem:Reminder) {
+        list.removeValueForKey(name)
+        var keyVal = Dictionary<String,AnyObject>()
+        keyVal.updateValue(rem.time, forKey: "time")
+        keyVal.updateValue(rem.title, forKey: "title")
+        keyVal.updateValue(rem.volume, forKey: "volume")
+        list.updateValue(keyVal, forKey: name)
+        let pathName = libPath.stringByAppendingPathComponent("reminders.dat")
+        do {
+            let data = try NSJSONSerialization.dataWithJSONObject(list, options: NSJSONWritingOptions())
+            data.writeToFile(pathName, atomically: true)
+        } catch {}
+    }
+    
+    func deleteReminderFromRepo(name:String) {
+        list.removeValueForKey(name)
+        let pathName = libPath.stringByAppendingPathComponent("reminders.dat")
+        do {
+            let data = try NSJSONSerialization.dataWithJSONObject(list, options: NSJSONWritingOptions())
+            data.writeToFile(pathName, atomically: true)
+        } catch {}
+    }
 }
